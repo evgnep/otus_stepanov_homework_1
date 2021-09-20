2d8018780ce657333a174f7ab1607861cfa235eb
Aleksey Stepanov <bssClient@gmail.com>
2021-09-20 23:40:35 +0500

    added index.ts

---
+++ index.test.ts
@@ -0,0 +1,7 @@
+import { TestMe } from ".";
+
+const testObj = new TestMe(1, 2);
+
+test("adds 1 + 2 to equal 3", () => {
+  expect(testObj.sum()).toBe(5);
+});
---
+++ index.ts
@@ -0,0 +1,6 @@
+export class TestMe {
+  constructor(public a, public b) {}
+  sum() {
+    return this.a + this.b;
+  }
+}
